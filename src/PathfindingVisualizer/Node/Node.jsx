4c7d123ee6c22312b008477f6973c3a34e8308b7
Andy <andyho66@gmail.com>
2024-03-15 12:24:22 -0700

    Developed a resizing feature, a start/finish node selector, and a reset function

--- src/PathfindingVisualizer/Node/Node.jsx
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -1,24 +1,26 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import './Node.css';
 
 function Node (props) {
+
   const extraClassName = props.isFinish
     ? 'node-finish'
     : props.isStart
     ? 'node-start'
     : props.isWall
     ? 'node-wall'
-    : '';
+    : 'node-plain';
+
+
 
   return (
     <div
-    id = {`node-${props.row}-${props.col}`}
-    className = {`node ${extraClassName}`}
-    onMouseDown={() => props.handleMouseDown(props.row, props.col)}
-    onMouseEnter={() => props.handleMouseEnter(props.row, props.col)}
-    onMouseUp={() => props.handleMouseUp(props.row, props.col)}
-    ></div>
+      id = {`node-${props.row}-${props.col}`}
+      className = {`node ${extraClassName}`}
+      onMouseDown={() => props.handleMouseDown(props.row, props.col)}
+      onMouseEnter={() => props.handleMouseEnter(props.row, props.col)}
+      onMouseUp={() => props.handleMouseUp(props.row, props.col)}></div>
   );
 }
 
--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,39 +1,190 @@
 import React, {useState, useEffect} from 'react';
 import Node from './Node/Node.jsx';
 import {dijkstra, getNodesInShortestPathOrder} from '../Algorithms/dijkstra';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 import './PathfindingVisualizer.css';
-
-const START_ROW = 10;
-const START_COL = 15;
-const FINISH_ROW = 10;
-const FINISH_COL = 35;
 
 function PathfindingVisualizer () {
   const [grid, setGrid] = useState([]);
   const [mouseIsPressed, setMouse] = useState(false);
+  const [startRow, setStartRow] = useState(10);
+  const [startCol, setStartCol] = useState(15);
+  const [finishRow, setFinishRow] = useState(10);
+  const [finishCol, setFinishCol] = useState(35);
+  const [rows, setRows] = useState(20);
+  const[cols, setCols] = useState(50);
+  const[resize, setResize] = useState(false);
+  const[pickStart, setPickStart] = useState(false);
+  const[pickFinish, setPickFinish] = useState(false);
+
+  const visualizeStyle = {
+    backgroundColor: '#265073',
+    fontFamily: 'roboto',
+    color: 'black',
+    fontSize: 20,
+    padding: '6px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+  };
+
+  const submitStyle = {
+    backgroundColor: '#265073',
+    fontFamily: 'roboto',
+    color: 'black',
+    fontSize: 16,
+    padding: '6px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+  };
+
+  const startStyle = {
+    backgroundColor: '#5F8670',
+    fontFamily: 'Roboto',
+    color: 'black',
+    fontSize: 16,
+    border: '1px solid',
+    lineHeight: 1.5,
+  };
+
+  const finishStyle = {
+    backgroundColor: '#820300',
+    fontFamily: 'Roboto',
+    color: 'black',
+    fontSize: 16,
+    border: '1px solid',
+    lineHeight: 1.5,
+  };
+
+  const resetStyle = {
+    backgroundColor: '#265073',
+    fontFamily: 'Roboto',
+    color: 'black',
+    fontSize: 16,
+    padding: '6px 12px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+  };
+
 
 
   useEffect (() => {
     const tempGrid = getGrid();
     setGrid(tempGrid);
-  }, []);
+  }, [resize]);
+
+  const onReset = function() {
+    for (let row = 0; row < rows; row++) {
+      for(let col = 0; col < cols; col++) {
+        const removeClass = document.getElementById(`node-${row}-${col}`);
+        if (removeClass.classList.length > 0) {
+          if (removeClass.classList.contains("node-finish")) removeClass.classList.remove("node-finish")
+          if (removeClass.classList.contains("node-start")) removeClass.classList.remove("node-start")
+          if (removeClass.classList.contains("node-visited")) removeClass.classList.remove("node-visited")
+          if (removeClass.classList.contains("node-shortest-path")) removeClass.classList.remove("node-shortest-path")
+        }
+      }
+    }
+    const newGrid = getGrid();
+    setGrid(newGrid);
+  }
+
+  const createNode = function(col, row) {
+    return {
+      col : col,
+      row : row,
+      isStart: row === startRow && col === startCol,
+      isFinish: row === finishRow && col === finishCol,
+      distance: Infinity,
+      isVisited: false,
+      isWall: false,
+      previousNode: null,
+    };
+  };
+
+  const getGrid = () => {
+    const grid = [];
+      for (let row = 0; row < rows; row++) {
+        const currRow = [];
+        for (let col = 0; col < cols; col++) {
+          currRow.push(createNode(col, row));
+        }
+        console.log(currRow)
+        grid.push(currRow);
+      }
+      return grid;
+  };
+
+  const resizeGrid = function(e) {
+    e.preventDefault()
+    setResize(!resize)
+  };
+
+  const onRows = function(e) {
+    setRows(e.target.value);
+  };
+
+  const onCols = function(e) {
+    setCols(e.target.value);
+  };
+
 
   const handleMouseDown = function(row, col) {
-    const newGrid = getNewGridWithWalls(grid, row, col);
-    setGrid(newGrid)
-    setMouse(true)
-  }
+    if (pickStart) {
+      if (rows > startRow && cols > startCol) {
+        grid[startRow][startCol].isStart = false;
+      }
+      setStartRow(row)
+      setStartCol(col)
+      setPickStart(false)
+      setResize(!resize)
+    } else if (pickFinish) {
+      if (rows > finishRow && cols > finishCol) {
+        grid[finishRow][finishCol].isStart = false;
+      }
+      setFinishRow(row)
+      setFinishCol(col)
+      setPickFinish(false)
+      setResize(!resize)
+    } else {
+      const newGrid = getNewGridWithWalls(grid, row, col);
+      setGrid(newGrid)
+      setMouse(true)
+    }
+  };
 
   const handleMouseEnter = function(row, col) {
     if (!mouseIsPressed) return;
     const newGrid = getNewGridWithWalls(grid, row, col);
     setGrid(newGrid)
-  }
+  };
 
   const handleMouseUp = function(row, col) {
     setMouse(false)
-  }
+  };
+
+  const onStartClick = function() {
+    setPickStart(true);
+  };
+
+  const onFinishClick = function() {
+    setPickFinish(true);
+  };
+
+  const selectStart = function(row, col) {
+    if (!pickStart) return;
+    setStartRow(row)
+    setStartCol(col)
+    setPickStart(false)
+  };
+
+  const selectFinish = function(row,col) {
+    if (!pickFinish) return;
+    setFinishRow(row)
+    setFinishCol(col)
+    setPickFinish(false)
+  };
 
   const animateDijkstra = function(visitedNodesInOrder, nodesInShortestPathOrder) {
     for (let i = 0; i <= visitedNodesInOrder.length; i++) {
@@ -45,16 +196,10 @@ function PathfindingVisualizer () {
       }
       setTimeout(() => {
         const node = visitedNodesInOrder[i];
-        // const newGrid = grid.slice();
-        // const newNode = {
-        //   ...node,
-        //   isVisited: true,
-        // };
-        // newGrid[node.row][node.col] = newNode;
         document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-visited';
       }, 10 * i)
     }
-  }
+  };
 
   const animateShortestPath = function(nodesInShortestPathOrder) {
     for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
@@ -67,17 +212,34 @@ function PathfindingVisualizer () {
 
   const visualizeDijkstra = function(e) {
     e.preventDefault()
-    const startNode = grid[START_ROW][START_COL];
-    const finishNode = grid[FINISH_ROW][FINISH_COL];
+    const startNode = grid[startRow][startCol];
+    const finishNode = grid[finishRow][finishCol];
     const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
     const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
     animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
   };
   return (
     <>
-      <button className="visualize" onClick = {visualizeDijkstra}>
-        Visualize Dijkstra's Algorithm
-      </button>
+      <div className="visualize-div">
+        <Button style={visualizeStyle} variant="contained" size="large" className="visualize" onClick = {visualizeDijkstra}>
+          Visualize Dijkstra's Algorithm
+        </Button>
+      </div>
+      <div className="submit-div">
+        <form className="form" onSubmit = {resizeGrid}>
+          <TextField variant="outlined" label="Number of Rows" placeholder='20' onChange={onRows}></TextField>
+          <TextField variant="outlined" label="Number of Columns" placeholder='50' onChange={onCols}></TextField>
+          <Button style={submitStyle}variant="contained" className="submit" type="submit">Confirm</Button>
+        </form>
+      </div>
+      <div className="select-node">
+        <Button style={startStyle} variant="contained" size="small" className="start-node" onClick = {onStartClick}>
+            Select a Start Node
+        </Button>
+        <Button style={finishStyle} variant="contained" size="small" className="finish-node" onClick = {onFinishClick}>
+            Select a Finish Node
+        </Button>
+      </div>
       <div className="grid">
         {grid.map((row, rowIndex) => {
           return (<div key={rowIndex}>
@@ -94,41 +256,23 @@ function PathfindingVisualizer () {
                   col = {col}
                   handleMouseDown = {handleMouseDown}
                   handleMouseEnter = {handleMouseEnter}
-                  handleMouseUp = {handleMouseUp}></Node>
+                  handleMouseUp = {handleMouseUp}
+                  selectStart =  {selectStart}
+                  selectFinish = {selectFinish}
+                  pickStart = {pickStart}
+                  pickFinish = {pickFinish}></Node>
               );
               })}
           </div>
           );
         })}
       </div>
+      <Button style={resetStyle} variant="contained" size="small" className="reset-button" onClick = {onReset}>
+          Reset
+      </Button>
     </>
   );
-}
-
-const getGrid = () => {
-  const grid = [];
-    for (let row = 0; row < 20; row++) {
-      const currRow = [];
-      for (let col = 0; col < 50; col++) {
-        currRow.push(createNode(col, row));
-      }
-      grid.push(currRow);
-    }
-    return grid;
-}
-
-const createNode = (col, row) => {
-  return {
-    col,
-    row,
-    isStart: row === START_ROW && col === START_COL,
-    isFinish: row === FINISH_ROW && col === FINISH_COL,
-    distance: Infinity,
-    isVisited: false,
-    isWall: false,
-    previousNode: null,
-  };
-}
+};
 
 const getNewGridWithWalls = (grid, row, col) =>{
   const newGrid = grid.slice();
@@ -139,7 +283,7 @@ const getNewGridWithWalls = (grid, row, col) =>{
   };
   newGrid[row][col] = newNode;
   return newGrid
-}
+};
 
 
 
